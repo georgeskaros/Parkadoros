3fc97a5a79aae05b7531a656276bbc0e384a3ee1
george-skar97 <george.skaros@hotmail.com>
2020-02-17 22:20:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/parkingfinder/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.parkingfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    //initiate button ,text view and two public variables for current
+    Button btn;
+    public double currentlat,currentlon;
+    TextView txt ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.btn);
+        txt = (TextView)findViewById(R.id.txt);
+
+        //asking for permission to use location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+        } else {
+            doStuff();
+
+        }
+        //initiate the function of the button
+        clickbtn();
+
+    }
+    private void clickbtn(){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //on button click print on text view the current values
+                txt.setText(currentlat +" "+ currentlon);
+            }
+        });
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //get on location change the current lat and lon
+        currentlat = location.getLatitude();
+        currentlon = location.getLongitude();
+    }
+
+    private void doStuff(){
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        if (lm != null){
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+            //commented, this is from the old version
+            // this.onLocationChanged(null);
+        }
+        Toast.makeText(this,"Waiting for GPS connection!", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
---
+++ app/src/main/java/com/example/parkingfinder/MapsActivity.java
@@ -0,0 +1,4 @@
+package com.example.parkingfinder;
+
+public class MapsActivity {
+}
