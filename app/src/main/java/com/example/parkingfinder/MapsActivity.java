85ef93cd5fd15878ef94a26e69e606d0a04c3a16
george-skar97 <george.skaros@hotmail.com>
2020-02-20 17:02:22 +0200

    marker making
    fixed the markers  that apear in the map

--- app/src/main/java/com/example/parkingfinder/MapsActivity.java
+++ app/src/main/java/com/example/parkingfinder/MapsActivity.java
@@ -42,8 +42,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     ArrayList<LatLng> listPoints;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private static final String TAG = MainActivity.class.getSimpleName();
-    double lat = 2.2;
-    double lng = 2.2;
+    double lat = -2.2;
+    double lng = -2.2;
     String vehicleType = " ";
     String radioBtnValue;
     Date minDate,maxDate;
@@ -83,80 +83,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.setMyLocationEnabled(true);
         //TODO make an if so if the radioBtnValue is "All parking lots" will print all the parking lots that are in the db
-        db.collection("locations")
-                .whereEqualTo("vehicleType",radioBtnValue)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-
-                        if (task.isSuccessful()) {
-
-                            //the following block of code is to find the biggest date that fits the where clause that i had put
-                            //first we take the current date and put it to a variable
-                            final Date currentDate = Calendar.getInstance().getTime();
-                            minDate = currentDate;
-                            //with this for loop we find the oldest date there is in our results
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Date dateTemp = document.getDate("time");
-                                if(minDate.compareTo(dateTemp)>0){
-                                    minDate = dateTemp;
-                                    lat = document.getDouble("latitude");
-                                    lng = document.getDouble("longitude");
-                                    vehicleType = document.getString("vehicleType");
+        if(!radioBtnValue.equals("All parking lots")) {
+            db.collection("locations")
+                    .whereEqualTo("vehicleType", radioBtnValue)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                //the following block of code is to find the biggest date that fits the where clause that i had put
+                                //first we take the current date and put it to a variable
+                                final Date currentDate = Calendar.getInstance().getTime();
+                                minDate = currentDate;
+                                //with this for loop we find the oldest date there is in our results
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Date dateTemp = document.getDate("time");
+                                    if (minDate.compareTo(dateTemp) > 0) {
+                                        minDate = dateTemp;
+                                        lat = document.getDouble("latitude");
+                                        lng = document.getDouble("longitude");
+                                        vehicleType = document.getString("vehicleType");
+                                    }
                                 }
-                            }
-                            //we put the minDate to a maxDate to do the reverse
-                            //when we find a date greater than maxDate we put that date on max date
-                            //and we store latitude, longitude, and vehicleType to some variables so we can use them later
-                            // to make a marker for the most resent entry of that vehicle type
-                            maxDate = minDate;
-                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                //we put the minDate to a maxDate to do the reverse
+                                //when we find a date greater than maxDate we put that date on max date
+                                //and we store latitude, longitude, and vehicleType to some variables so we can use them later
+                                // to make a marker for the most resent entry of that vehicle type
+                                maxDate = minDate;
+                                for (QueryDocumentSnapshot document : task.getResult()) {
 
-                                Date dateTemp = document.getDate("time");
-                                if(maxDate.compareTo(dateTemp)<0){
-                                    Toast.makeText(MapsActivity.this, "ok", Toast.LENGTH_SHORT).show();
-                                    maxDate = dateTemp;
-                                    lat = document.getDouble("latitude");
-                                    lng = document.getDouble("longitude");
-                                    vehicleType = document.getString("vehicleType");
+                                    Date dateTemp = document.getDate("time");
+                                    if (maxDate.compareTo(dateTemp) < 0) {
+                                        Toast.makeText(MapsActivity.this, "ok", Toast.LENGTH_SHORT).show();
+                                        maxDate = dateTemp;
+                                        lat = document.getDouble("latitude");
+                                        lng = document.getDouble("longitude");
+                                        vehicleType = document.getString("vehicleType");
+                                    }
                                 }
-                            }
-                            //TODO make an IF so it will check if latLng is (0,0) an will not process the marker
-                            LatLng location = new LatLng(lat,lng);
-                            MarkerOptions markerOptions = new MarkerOptions();                   //create marker options class so we can ad features to it
-                            markerOptions.position(location);
-                            markerOptions.title(vehicleType);                                   //setting a title to the marker , now the title is
-                            markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.car));
-                            Marker locationMarker = mMap.addMarker(markerOptions);
-                            locationMarker.setDraggable(true);
-                            locationMarker.showInfoWindow();
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,14.0f));
+                                if(lat>0 && lng>0){
+                                    //TODO make an IF so it will check if latLng is (0,0) an will not process the marker
+                                    LatLng location = new LatLng(lat, lng);
+                                    MarkerOptions markerOptions = new MarkerOptions();                   //create marker options class so we can ad features to it
+                                    markerOptions.position(location);
+                                    markerOptions.title(vehicleType);                                   //setting a title to the marker , now the title is
+                                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.car));
+                                    Marker locationMarker = mMap.addMarker(markerOptions);
+                                    locationMarker.setDraggable(true);
+                                    locationMarker.showInfoWindow();
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 14.0f));
+                                }else{
+                                    Toast.makeText(MapsActivity.this, "There isn't any vehicle of this type parked", Toast.LENGTH_SHORT).show();
+                                }
 
-                            ///////////////////////////////////////////////////////
-                            //code to use in order to print all entries to the DB//
-                            ///////////////////////////////////////////////////////
-                            /*for (QueryDocumentSnapshot document : task.getResult()) {
-                               // if (document.getLong("time")>maxTime){Toast.makeText(MapsActivity.this, "ok", Toast.LENGTH_SHORT).show();}else{
-                                LatLng location = new LatLng(document.getDouble("latitude"),document.getDouble("longitude"));       //get location from db
+                            } else {
+                                Toast.makeText(MapsActivity.this, "Can't find a saved location", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }else{
+            db.collection("locations")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                LatLng location = new LatLng(document.getDouble("latitude"), document.getDouble("longitude"));       //get location from db
                                 String vehicle = document.getString("vehicleType");
                                 MarkerOptions markerOptions = new MarkerOptions();                   //create marker options class so we can ad features to it
                                 markerOptions.position(location);
                                 markerOptions.title(vehicle);               //setting a title to each of the markers , now the title is the LatLng
                                 markerOptions.snippet("comment");               //can put anything in here , this is like a comment to the title
                                 markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.car));       //set a car icon
-                                //Marker locationmarker = mMap.addMarker(new MarkerOptions().position(location));
                                 Marker locationMarker = mMap.addMarker(markerOptions);
                                 locationMarker.setDraggable(true);
                                 locationMarker.showInfoWindow();
                                 mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,14.0f));
-                            }*/
-                        } else {
-                            Toast.makeText(MapsActivity.this, "Can't find a saved location", Toast.LENGTH_SHORT).show();
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 14.0f));
+                            }
                         }
-                    }
-                });
+                    });
+        }
     }
 }
